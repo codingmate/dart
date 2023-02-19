3099b085c11b7031be95f60351c052f746801607
sy-playground <87705358+codingmate@users.noreply.github.com>
2023-02-20 08:57:20 +0900

    Create temp.dart

---
+++ temp.dart
@@ -0,0 +1,42 @@
+void main() {
+  print('Hello World');
+  print('"Hello World"');
+  
+  print("Hello World");
+  print("'Hello World'");
+  
+  var id = "id001";
+  print(id);
+  
+  id = "id999";
+  print(id);
+  
+  int num001 = 1;
+  print(num001);
+    
+  int num002 = 2;
+  print(num002);
+  print(num001 + num002);
+  print(num001 * num002);
+  print(num001 / num002);
+  print(num001 - num002);
+  
+  double num004 = 2.2;
+  double num005 = 1.1;
+  
+  
+}
+/*
+Hello World
+"Hello World"
+Hello World
+'Hello World'
+id001
+id999
+1
+2
+3
+2
+0.5
+-1
+*/
